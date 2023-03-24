06b008312cc414705a7ac3a946e264a66c31be52
root <c.onuoha@alustudent.com>
2023-03-24 06:39:52 -0700

    More tasks

---
+++ python-input_output/100-append_after.py
@@ -0,0 +1,27 @@
+#!/usr/bin/python3
+"""Write a function that inserts a line of text to a file,
+after each line containing a specific string (see example):
+Prototype: def append_after(filename="", search_string="", new_string=""):
+You must use the with statement
+You don’t need to manage file permission or file doesn't exist exceptions.
+You are not allowed to import any module"""
+
+
+def append_after(filename="", search_string="", new_string=""):
+    """
+    Append a new line when a string is found
+    Args:
+        filename: filename
+        search_string: string to search
+        new_string: string to append
+    """
+
+    res_line = []
+    with open(filename, 'r', encoding="utf-8") as f:
+        for line in f:
+            res_line += [line]
+            if line.find(search_string) != -1:
+                res_line += [new_string]
+
+    with open(filename, 'w', encoding="utf-8") as f:
+        f.write("".join(res_line))
